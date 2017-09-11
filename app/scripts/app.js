b4758776c04a19ac9207d06656a1f0cb0eea3c43
Jenny Hong <yjhong1004@gmail.com>
2017-09-11 15:21:08 -0700

    checkpoint-routing changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,25 @@
-angular.module('blocJams', []);
+(function() {
+     function config($stateProvider, $locationProvider) {
+       $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+        $stateProvider
+            .state('landing', {
+              url: '/',
+              templateUrl: '/templates/landing.html'
+            })
+            .state('collection', {
+              url: '/collection',
+              templateUrl: '/templates/collection.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
+     }
+     angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
