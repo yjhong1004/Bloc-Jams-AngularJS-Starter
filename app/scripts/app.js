ef542739b064f86e25bc7c4c861c2ef4555f426f
Jenny Hong <yjhong1004@gmail.com>
2017-09-13 10:49:21 -0700

    checkpoint-controllers changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,10 +8,12 @@
         $stateProvider
             .state('landing', {
               url: '/',
+              controller: 'LandingCtrl as landing',
               templateUrl: '/templates/landing.html'
             })
             .state('collection', {
               url: '/collection',
+              controller: 'CollectionCtrl as collection',
               templateUrl: '/templates/collection.html'
             })
             .state('album', {
