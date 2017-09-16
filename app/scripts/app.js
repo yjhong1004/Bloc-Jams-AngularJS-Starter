26ef2b9df78f56a952c61bb2c6cf3b446079a6f5
Jenny Hong <yjhong1004@gmail.com>
2017-09-16 12:15:14 -0700

    assignment-controllers changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,6 +18,7 @@
             })
             .state('album', {
                 url: '/album',
+                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
             });
      }
