87f53e1b260b82eba4b463c88390c45f6010fe12
Jenny Hong <yjhong1004@gmail.com>
2017-09-19 00:53:42 -0700

    checkpoint-services1 changes

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,10 @@
 (function() {
      function CollectionCtrl() {
-       this.albums = [];
-       for (var i=0; i < 12; i++) {
-           this.albums.push(angular.copy(albumPicasso));
-       }
+       this.albums = Fixtures.getCollection(12);
+      //  this.albums = [];
+      //  for (var i=0; i < 12; i++) {
+      //      this.albums.push(angular.copy(albumPicasso));
+      //  }
      }
 
      angular
