6f441b6bd03c1f7a32d92a3c044aaf1c37195364
Jenny Hong <yjhong1004@gmail.com>
2017-10-03 12:31:15 -0700

    assignment-directives1 changes

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -32,6 +32,10 @@
                return {width: percentString()};
            };
 
+          //  scope.thumbStyle = function () {
+          //     return
+          //  };
+
            scope.onClickSeekBar = function(event) {
              var percent = calculatePercent(seekBar, event);
              scope.value = percent * scope.max;
@@ -40,9 +44,9 @@
          scope.trackThumb = function() {
              $document.bind('mousemove.thumb', function(event) {
                var percent = calculatePercent(seekBar, event);
-               scope.$apply(function() {
+              //  scope.$apply(function() {
                  scope.value = percent * scope.max;
-               });
+              //  });
              });
 
              $document.bind('mouseup.thumb', function() {
