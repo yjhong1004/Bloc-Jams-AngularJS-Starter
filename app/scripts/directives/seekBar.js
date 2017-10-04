7db210f621641d6f8a5be2abca9ae9a54dc9bb4f
Jenny Hong <yjhong1004@gmail.com>
2017-10-04 13:48:13 -0700

    assignment-directives1 change

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -13,12 +13,21 @@
          templateUrl: '/templates/directives/seek_bar.html',
          replace: true,
          restrict: 'E',
-         scope: { },
+         scope: {
+           onChange: '&'
+         },
          link: function(scope, element, attributes) {
            scope.value = 0;
            scope.max = 100;
 
            var seekBar = $(element);
+           attributes.$observe('value', function(newValue) {
+             scope.value = newValue;
+           });
+
+           attributes.$observe('max', function(newValue) {
+             scope.max = newValue;
+           });
 
 
            var percentString = function () {
@@ -32,27 +41,34 @@
                return {width: percentString()};
            };
 
-          //  scope.thumbStyle = function () {
-          //     return
-          //  };
+           scope.thumbStyle = function () {
+              return {left: percentString()};
+           };
 
            scope.onClickSeekBar = function(event) {
              var percent = calculatePercent(seekBar, event);
              scope.value = percent * scope.max;
+             notifyOnChange(scope.value);
          };
 
          scope.trackThumb = function() {
              $document.bind('mousemove.thumb', function(event) {
                var percent = calculatePercent(seekBar, event);
-              //  scope.$apply(function() {
+               scope.$apply(function() {
                  scope.value = percent * scope.max;
-              //  });
+                 notifyOnChange(scope.value);
+               });
              });
 
              $document.bind('mouseup.thumb', function() {
                $document.unbind('mousemove.thumb');
                $document.unbind('mouseup.thumb');
              });
+        };
+        var notifyOnChange = function(newValue) {
+          if (typeof scope.onChange === 'function') {
+            scope.onChange({value: newValue});
+          }
         };
          }
      };
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,6 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
+
           /**
           * @desc Buzz object audio file
           * @type {Object}
@@ -25,6 +26,12 @@
                  preload: true
              });
 
+             currentBuzzObject.bind('timeupdate', function() {
+               $rootScope.$apply(function() {
+                 SongPlayer.currentTime = currentBuzzObject.getTime();
+               });
+             });
+
              SongPlayer.currentSong = song;
           };
 
@@ -39,6 +46,14 @@
           };
 
           SongPlayer.currentSong = null;
+          /**
+          * @desc Current playback time (in seconds) of currently playing song
+          * @type {Number}
+          */
+          SongPlayer.currentTime = null;
+
+          SongPlayer.volume = 80;
+
           SongPlayer.play = function(song) {
             song = song || SongPlayer.currentSong;
             if (SongPlayer.currentSong !== song) {
@@ -82,7 +97,22 @@
             }
 
           };
+          /**
+           * @function setCurrentTime
+           * @desc Set current time (in seconds) of currently playing song
+           * @param {Number} time
+           */
+           SongPlayer.setCurrentTime = function(time) {
+               if (currentBuzzObject) {
+                   currentBuzzObject.setTime(time);
+               }
+           };
 
+           SongPlayer.setVolume = function(volume){
+             if (currentBuzzObject) {
+               currentBuzzObject.setVolume(volume);
+             }
+           }
           var getSongIndex = function(song) {
             return currentAlbum.songs.indexOf(song);
           };
@@ -92,5 +122,5 @@
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
